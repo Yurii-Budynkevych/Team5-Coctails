f76746d885cb65bcddc1eb286b11192355415a95
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-10-12 16:57:46 +0300

    joined

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,113 +1,140 @@
 'use strict';
 const refst = {
-    heroList: document.querySelector('.hero-list'),
-    heroTitle: document.querySelector('.hero-text'),
-    hero: document.querySelector('.hero'),
-    select: document.querySelector('.hero-select'),
-    isHiden: document.querySelector('.is-hiden'),
-    heroItem: document.querySelectorAll('.hero-item'),
-}
-const {heroList, heroTitle,hero,select,isHiden,heroItem} = refst
+  heroList: document.querySelector('.hero-list'),
+  heroTitle: document.querySelector('.hero-text'),
+  hero: document.querySelector('.hero'),
+  select: document.querySelector('.hero-select'),
+  isHiden: document.querySelector('.is-hiden'),
+  heroItem: document.querySelectorAll('.hero-item'),
+};
+import mainFunction from './coctails';
 
+let arrayLength = 0;
 
-const heroTitleImg = () =>{
-    return `<div class="hero-container">
-</div>`
-}
+const { heroList, heroTitle, hero, select, isHiden, heroItem } = refst;
 
-function creaitMarkapArr(e) { 
-return e.map(e => {
+const heroTitleImg = () => {
+  return `<div class="hero-container">
+</div>`;
+};
+
+function creaitMarkapArr(e) {
+  return e.map(e => {
     return `<li class=hero-item >
         <button class=hero-button data-name=${e}>${e}</button>
-        </li>`
+        </li>`;
+  });
+}
+
+const arrr = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+];
+
+function creaitMarkap(e) {
+  return e.map(e => {
+    return `<option class=hero-item value=${e}>${e}</option>`;
+  });
+}
+
+function clickHeroTitel(e) {
+  const target = e.target.dataset.name;
+  const hover = e.target;
+  const item = e.currentTarget.querySelectorAll('.hero-button');
+  if (innerWidth < 767) {
+    const hoverTarget = e.target.value;
+    cocktalis(hoverTarget).then(data => {
+      console.log(data);
     });
-    }
-    
-    const arrr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
+  }
+  if (!target) {
+    return;
+  }
+  if (innerWidth > 767) {
+    // item.forEach(e => {
+    //     if(!e.classList.contains('is-hover')){return}
+    //     e.classList.remove('is-hover')
+    // })
 
-    function creaitMarkap(e) { 
-      
-        return e.map(e => {
-         return `<option class=hero-item value=${e}>${e}</option>`
-        });
-        }
-      
-      
-      
-      
-        function clickHeroTitel(e){
-        const target = e.target.dataset.name
-        const hover = e.target
-        const item = e.currentTarget.querySelectorAll(".hero-button")
-        if(innerWidth < 767){
-            const hoverTarget = e.target.value
-            cocktalis(hoverTarget).then(data=>{
-                console.log(data)
-            });
-           }
-           if(!target){
-            return
-        }
-        if(innerWidth > 767){
+    try {
+      const removeTarget = document.querySelector('.is-hover');
 
+      removeTarget.classList.remove('is-hover');
+      console.log(removeTarget);
+      item.classList.add('is-hover');
+    } catch {}
+    console.log(hover);
+    hover.classList.add('is-hover');
 
-            // item.forEach(e => {
-            //     if(!e.classList.contains('is-hover')){return}
-            //     e.classList.remove('is-hover')
-            // })
-        
-             try{
-            const removeTarget = document.querySelector('.is-hover');
-      
-            removeTarget.classList.remove("is-hover")
-            console.log(removeTarget);
-            item.classList.add("is-hover")
-        }catch{}
-            console.log(hover);
-            hover.classList.add("is-hover")
-    
-             cocktalis(target).then(data=>{
-                const {drinks} = data
-             
-                 console.log(data)
-             });
-            }
+    cocktalis(target);
+  }
+}
 
-      }
+heroList.addEventListener('click', clickHeroTitel);
 
+const hiden = creaitMarkap(arrr);
+const markap = creaitMarkapArr(arrr);
+const heroWidth = heroTitleImg();
 
+if (innerWidth > 767) {
+  isHiden.classList.add('is-hiden');
+  heroList.insertAdjacentHTML('beforeend', markap.join(''));
+  hero.insertAdjacentHTML('beforeend', heroWidth);
+}
 
-    
-        heroList.addEventListener('click',clickHeroTitel)
-
-const hiden = creaitMarkap(arrr)
-const markap = creaitMarkapArr(arrr)
- const heroWidth = heroTitleImg()
-
-if(innerWidth > 767){
-    isHiden.classList.add('is-hiden')
-    heroList.insertAdjacentHTML('beforeend',markap.join(''))
-    hero.insertAdjacentHTML('beforeend', heroWidth)
-};
-
-if(innerWidth < 767){
-    isHiden.classList.remove('is-hiden')
-    heroTitle.insertAdjacentHTML('beforebegin', heroWidth)
-    select.insertAdjacentHTML('beforeend',hiden.join(''))
-};
-
-
-
-
+if (innerWidth < 767) {
+  isHiden.classList.remove('is-hiden');
+  heroTitle.insertAdjacentHTML('beforebegin', heroWidth);
+  select.insertAdjacentHTML('beforeend', hiden.join(''));
+}
 
 function cocktalis(name) {
-    const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${name}`
-    return fetch(URL).then(response =>{
-        if(!response){
-            throw new Error(response.message)
-        }
-        return response.json()
-    })
-    }
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${name}`;
+  return fetch(URL)
+    .then(response => {
+      if (!response) {
+        throw new Error(response.message);
+      }
 
-    
+      return response.json();
+    })
+    .then(response => {
+      arrayLength = response.drinks.length;
+      console.log(arrayLength);
+      mainFunction(1, URL, arrayLength);
+    });
+}
