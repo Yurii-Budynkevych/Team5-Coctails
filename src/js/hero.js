9ad1e60b4e137aba5b535a429dcc82026f87a002
Zhe1a <mgr58006@gmail.com>
2022-10-12 12:10:43 +0300

    ++++

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,19 +1,104 @@
 'use strict';
+const refst = {
+    heroList: document.querySelector('.hero-list'),
+    heroTitle: document.querySelector('.hero-text'),
+    hero: document.querySelector('.hero'),
+    select: document.querySelector('.hero-select'),
+    isHiden: document.querySelector('.is-hiden'),
+    heroItem: document.querySelectorAll('.hero-item'),
+}
+const {heroList, heroTitle,hero,select,isHiden,heroItem} = refst
+
+
+const heroTitleImg = () =>{
+    return `<div class="hero-container">
+</div>`
+}
+
 function creaitMarkapArr(e) { 
-    return e.map(e => {
-        return `<li class=hero__span><span data-name=${e}>${e}</span></li>`
+return e.map(e => {
+    return `<li class=hero-item >
+        <button class=hero-button data-name=${e}>${e}</button>
+        </li>`
     });
     }
     
     const arrr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
-    const dd = document.querySelector('.hero-list');
-    function name(arrr) {
+
+    function creaitMarkap(e) { 
       
-        const markap = creaitMarkapArr(arrr)
-        dd.insertAdjacentHTML('beforeend',markap.join(''))
-    //  dd.innerHTML = markap.join('')
-    }
+        return e.map(e => {
+         return `<option class=hero-item value=${e}>${e}</option>`
+        });
+        }
+      
+      
+      
+      
+        function clickHeroTitel(e){
+        const target = e.target.dataset.name
+        const hover = e.target
+        const item = e.currentTarget.querySelectorAll(".hero-button")
+        if(innerWidth < 767){
+            const hoverTarget = e.target.value
+            cocktalis(hoverTarget).then(data=>{
+                console.log(data)
+            });
+           }
+           if(!target){
+            return
+        }
+        if(innerWidth > 767){
+            item.forEach(e => {
+                if(!e.classList.contains('is-hover')){return}
+                e.classList.remove('is-hover')
+            })
+         
+            console.log(hover);
+            hover.classList.add("is-hover")
     
-    name(arrr)
+             cocktalis(target).then(data=>{
+                const {drinks} = data
+             
+                 console.log(data)
+             });
+            }
+
+      }
+
+
+
+    
+        heroList.addEventListener('click',clickHeroTitel)
+
+const hiden = creaitMarkap(arrr)
+const markap = creaitMarkapArr(arrr)
+ const heroWidth = heroTitleImg()
+
+if(innerWidth > 767){
+    isHiden.classList.add('is-hiden')
+    heroList.insertAdjacentHTML('beforeend',markap.join(''))
+    hero.insertAdjacentHTML('beforeend', heroWidth)
+};
+
+if(innerWidth < 767){
+    isHiden.classList.remove('is-hiden')
+    heroTitle.insertAdjacentHTML('beforebegin', heroWidth)
+    select.insertAdjacentHTML('beforeend',hiden.join(''))
+};
+
+
+
+
+
+function cocktalis(name) {
+    const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${name}`
+    return fetch(URL).then(response =>{
+        if(!response){
+            throw new Error(response.message)
+        }
+        return response.json()
+    })
+    }
 
     
