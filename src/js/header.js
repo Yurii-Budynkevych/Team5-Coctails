fdf3d92a36fb13347ccf3b2168d1aec7f9241846
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-10-13 16:22:38 +0300

    header fix

--- src/js/header.js
+++ src/js/header.js
@@ -8,13 +8,13 @@ const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/';
 const DEBOUNCE_DELAY = 500;
 const refs = {
   input: document.querySelector('.header-input'),
-  mobileInput: document.querySelector('header-input-mobile'),
   btn: document.querySelector('.menu-batton'),
   mobileMenu: document.querySelector('.header-mobile-menu'),
+  list: document.querySelectorAll('.is-hidden'),
+  lisBtn: document.querySelector('.favorite-btn-desctop'),
 };
 
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-// refs.mobileInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(evt) {
   let serchQuery = evt.target.value;
   fetch(`${BASE_URL}search.php?s=${serchQuery}`)
@@ -39,4 +39,24 @@ function onClick() {
   refs.btn.classList.toggle('is-open');
   refs.mobileMenu.classList.toggle('is-open');
   refs.btn.setAttribute('aria-expanded', !expanded);
+
+  const mobileInput = document.querySelector('.header-input-mobile');
+  mobileInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+  const mobileFovoriteBtn = document.querySelector('.favorite-btn');
+  const mobileList = document.querySelectorAll('.is-hidden');
+  mobileFovoriteBtn.addEventListener('click', onFavoriteClick);
+  function onFavoriteClick() {
+    mobileList.forEach(element => {
+      element.classList.toggle('is-hidden');
+    });
+  }
+}
+// ______________________________________________________
+// favourite btn
+refs.lisBtn.addEventListener('click', onFavoriteDescktopClick);
+function onFavoriteDescktopClick() {
+  refs.list.forEach(el => {
+    el.classList.toggle('is-hidden');
+  });
 }
